ceb90faca536af04c400708cd07b8c4b70f9a2e5
Honey <honey@Honeyui-MacBook-Pro.local>
2017-10-31 05:24:35 +0900

    Basic O2O Function add.
    - change settings.py and urls.py in postech
    - add some basic code in O2O directory

--- postech/settings.py
+++ postech/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5z9$(la)&uk=&%l6ma#@0(iz6)777421m=44ruq@55m*0hm@#='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'O2O',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul' 
 
 USE_I18N = True
 

31bdbd1b27f4530c5ee498cfadf7277a53c8154e
kevins3204 <kevins3204@gmail.com>
2017-11-14 02:54:02 +0900

    models added

---
+++ O2O/EventHolder/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Person(models.Model):
+    user_nickname = models.CharField(max_length = 50)
+    user_id = models.CharField(max_length = 50)
+    
+class Poster(models.Model):
+    holder = models.ForeignKey(Person)
+    uploaddate = models.DateTimeField(
+            default=timezone.now)
+    eventname = models.CharField(max_length = 50)
+    eventdate = models.DateTimeField(
+            blank=True, null=True)
+    eventtext = models.TextField()

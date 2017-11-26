9ca2546a8c8bb9c10ed75098718919168549d958
Honey <honey@Honeyui-MacBook-Pro.local>
2017-11-27 01:01:54 +0900

    Google calendar permission solved  - remove .credential's json file and make new json with quickstart.py

--- O2O/calendar/quickstart.py
+++ O2O/calendar/quickstart.py
@@ -18,7 +18,7 @@ except ImportError:
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/calendar-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+SCOPES = 'https://www.googleapis.com/auth/calendar'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Google Calendar API Python Quickstart'
 
@@ -60,9 +60,12 @@ def main():
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
-
+    
+    '''
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    
     print('Getting the upcoming 10 events')
+    
     eventsResult = service.events().list(
         calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
         orderBy='startTime').execute()
@@ -73,6 +76,38 @@ def main():
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(start, event['summary'])
+    '''
+    
+    event = {
+      'summary': 'Example I/O 2017',
+      'location': 'My home',
+      'description': 'A chance to hear more about Google\'s developer products.',
+      'start': {
+        'dateTime': '2017-11-25T09:00:00',
+        'timeZone': 'Asia/Seoul',
+      },
+      'end': {
+        'dateTime': '2017-11-25T17:00:00',
+        'timeZone': 'Asia/Seoul',
+      },
+      'recurrence': [
+        #'RRULE:FREQ=DAILY;COUNT=2'
+      ],
+      'attendees': [
+        #{'email': 'lpage@example.com'},
+        #{'email': 'sbrin@example.com'},
+      ],
+      'reminders': {
+        'useDefault': False,
+        'overrides': [
+          #{'method': 'email', 'minutes': 24 * 60},
+          #{'method': 'popup', 'minutes': 10},
+        ],
+      },
+    }
+    
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print ('Event created: %s' % (event.get('htmlLink')))
 
 
 if __name__ == '__main__':
